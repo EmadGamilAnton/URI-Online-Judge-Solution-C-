8403b4c7e76e345bd3b703ffda95254fa2da3d41
EMAD GAMEL ANTON <emad.gamil126@el-eng.menofia.edu.eg>
2018-02-28 12:55:32 +0200

    ok
    ok

--- URI Judge Online/1071/main.cpp
+++ URI Judge Online/1071/main.cpp
@@ -6,14 +6,32 @@ using namespace std;
 int main()
 {
 
-    int inputNumber = 0;
-    cin>>inputNumber;
-    for(int i=inputNumber; i<inputNumber+12; i++)
-    {
-        if(i%2 != 0){
-            cout<<i<<endl;
-        }
+    int inputNumberX = 0;
+    int inputNumberY = 0;
+    int sum=0;
 
+    cin>>inputNumberX>>inputNumberY;
+
+
+    if(inputNumberX == inputNumberY)
+        cout<<sum<<endl;
+    else if(inputNumberX < inputNumberY)
+    {
+        for(inputNumberX=inputNumberX+1;inputNumberX<inputNumberY;inputNumberX++)
+        {
+            if(inputNumberX%2==1||inputNumberX%2==-1)
+                sum+=inputNumberX;
+        }
+        cout<<sum<<endl;
+    }
+    else if(inputNumberX>inputNumberY)
+    {
+        for(inputNumberY=inputNumberY+1;inputNumberY<inputNumberX;inputNumberY++)
+        {
+            if(inputNumberY%2==1||inputNumberY%2==-1)
+                sum+=inputNumberY;
+        }
+        cout<<sum<<endl;
     }
     return 0;
 }
